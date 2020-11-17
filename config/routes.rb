686623589c929a297f595bfa6ef4e5518595b4aa
DICE-K22 <d.aikawa22@gmail.com>
2020-11-17 11:39:20 +0900

    削除機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   root to: "reviews#index"
-  resources :reviews, only: [:index, :new, :create, :show]
+  resources :reviews, only: [:index, :new, :create, :show, :destroy]
 end
